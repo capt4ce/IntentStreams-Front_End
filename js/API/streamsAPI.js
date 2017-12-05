40991d10193504ac945044fa49f5e2274f4d3052
Capt.Ace <capt4ce@localhost.localdomain>
2017-12-06 00:16:12 +0530

    hadling google api request on progress

--- js/API/streamsAPI.js
+++ js/API/streamsAPI.js
@@ -55,58 +55,58 @@ module.exports = {
         // ];
 
         // FluxCartActions.loadStuffs(post, keyword);
-        // axios.get('http://localhost:5000/search', {
-        //     params: {
-        //         query: query
-        //     }
-        // })
-        //     .then(function (response) {
-        //         console.log(response)
-        //         console.log(response.data.success)
-        //         if (response.data.success == true) {
-        //             FluxCartActions.loadStuffs(response.data.data, response.data.suggested_keywords);
-        //         }
-
-        //     })
-
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
-
-        var data=[]
-        var ky=[]
-        axios.get('https://googleapis.com/customsearch/v1', {
-                params: {
-                    q: query,
-                    key: 'AlzaSyASWpsbC2yHZZA71001234642073522909578:dw7qnkw1euk'
+        axios.get('http://localhost:5000/search', {
+            params: {
+                query: query
+            }
+        })
+            .then(function (response) {
+                console.log(response)
+                console.log(response.data.success)
+                if (response.data.success == true) {
+                    FluxCartActions.loadStuffs(response.data.data, response.data.suggested_keywords);
                 }
+
             })
-                .then(function (response) {
-                    console.log(response)
-                    for (itm in response.items)
-                        data.push({name:itm.link,description:itm.snippet})
-                })
-                .then(function(){
-                    axios.get('http://api.bing.com/osjson.aspx', {
-                        params: {
-                            q: query                        
-                        }
-                    })
-                        .then(function (response) {
-                            console.log(response)
-                            for (i=0; i<5, response[1].length; i++)
-                                ky.push({name:response[1][i]})
-                        })
-                        .then(function(){
-                            FluxCartActions.loadStuffs(data, ky);
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        })
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        // var data=[]
+        // var ky=[]
+        // axios.get('https://googleapis.com/customsearch/v1', {
+        //         params: {
+        //             q: query,
+        //             key: 'AlzaSyASWpsbC2yHZZA71001234642073522909578:dw7qnkw1euk'
+        //         }
+        //     })
+        //         .then(function (response) {
+        //             console.log(response)
+        //             for (itm in response.items)
+        //                 data.push({name:itm.link,description:itm.snippet})
+        //         })
+        //         .then(function(){
+        //             axios.get('http://api.bing.com/osjson.aspx', {
+        //                 params: {
+        //                     q: query                        
+        //                 }
+        //             })
+        //                 .then(function (response) {
+        //                     console.log(response)
+        //                     for (i=0; i<5, response[1].length; i++)
+        //                         ky.push({name:response[1][i]})
+        //                 })
+        //                 .then(function(){
+        //                     FluxCartActions.loadStuffs(data, ky);
+        //                 })
+        //                 .catch(function (error) {
+        //                     console.log(error);
+        //                 })
+        //         })
+        //         .catch(function (error) {
+        //             console.log(error);
+        //         });
 
 
 
